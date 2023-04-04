480b021df8e9eff9bf48e1f7c038f4e5a1783946
michelxd456 <michelbalvinor@gmail.com>
2023-04-03 19:21:47 -0500

    actualizacion del Json 03/04/2023

--- proyecto.js
+++ proyecto.js
@@ -18,16 +18,46 @@ const puppeteer = require('puppeteer');
 
   const jsonData = await page.evaluate(()=>{
     const trendsList = document.querySelectorAll('.GsBXsKvH > .u4c2Cda9 div h3 a');
-    const jsonData = [];
-
-    for (let i = 0; i < trendsList.length; i++) {
-      const title = trendsList[i].innerHTML;
-      const dive = 'https://elements.envato.com' + trendsList[i].getAttribute('href');
+    const conteiner = document.querySelectorAll('.tXwhn5Zg div[data-test-selector="photogrid-photo"]');
+    const tituloVideo = document.querySelectorAll('.GsBXsKvH  > .u4c2Cda9  ul li a');
     
+    const jsonData = [];
+    
+    for (let i = 0; i < trendsList.length; i++) {
+      const url = 'https://elements.envato.com';
+      const title = trendsList[i].innerHTML;
+      const dive = url + trendsList[i].getAttribute('href');
+    
+      let containerData;
+      if (i === 0) {
+        
+       /* ,descripcion:[child.getAttribute('aria-label').replace('See more details for ',''),'https://elements.envato.com' +child.getAttribute('href')] */
+       let elementAutor = '.trzLbgZD div .Ago_n9Jb a';
+       let elementDescrip = '.SZBxAOrq';
+       let elementImagen = ' .NgtXrCQY img';
+        containerData = Array.from(conteiner).map(child => [
+            {autor: [child.querySelector(elementAutor).innerHTML,
+              url + child.querySelector(elementAutor).getAttribute('href')],
+
+            descripcion:[child.querySelector(elementDescrip).getAttribute('aria-label').replace('See more details for ',''),
+                        url +child.querySelector(elementDescrip).getAttribute('href'),
+                      child.querySelector(elementImagen).getAttribute('src'),
+                    child.querySelector(elementImagen).getAttribute('srcset')]}]
+          );
+
+       
+      } else {
+        containerData = Array.from(tituloVideo).map(item => [
+          item.textContent.trim(),
+          item.href,
+          item.querySelector('div[title="Author"]')?.getAttribute('title') || '',
+          item.querySelector('div[title="Author"] a')?.href || '',
+        ]);
+      }
     
       const data = {
         "title": [title, dive],
-        "contenedores": [["title", "creator", "linkDescarga"],["title", "creator", "linkDescarga"]]
+        "container": containerData
       };
     
       jsonData.push(data);
@@ -36,8 +66,11 @@ const puppeteer = require('puppeteer');
     return jsonData;
     
     
+    
+    
   })
   console.log(jsonData);
+  console.log(jsonData[0].container[0])
   
 
   //await browser.close();
